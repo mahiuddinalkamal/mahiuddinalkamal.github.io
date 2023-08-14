661cf5293758e5ac9db7ecd280ba8cf166a62718
Mahiuddin Al Kamal <contact@mahiuddinalkamal.com>
2023-08-14 11:50:22 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -24,7 +24,7 @@ const config = {
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://dev.mahiuddinalkamal.com',
+    website: 'https://mahiuddinalkamal.com',
     phone: '+49-1789291398',
     email: 'contact@mahiuddinalkamal.com',
   },
